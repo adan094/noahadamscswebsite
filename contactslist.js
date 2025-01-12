130e0fbc8ae62c8de8f3903f0071b53308c453aa
adan094 <adams128@uwindsor.ca>
2025-01-11 23:43:22 -0500

    prod

--- contactslist.js
+++ contactslist.js
@@ -1,22 +1,22 @@
 export default [
     {
         text: "LinkedIn",
-        src: "../src/linkedin.svg",
-        srcHover: "../src/linkedinblue.svg",
+        src: "./linkedin.svg",
+        srcHover: "./linkedinblue.svg",
         alt: "LinkedIn Logo",
         action:   function action (){window.open('https://www.linkedin.com/in/noahadamscs/')}
     },
     {
         text: "GitHub",
-        src: "../src/github.svg",
-        srcHover: "../src/githubblue.svg",
+        src: "./github.svg",
+        srcHover: "./githubblue.svg",
         alt: "GitHub Logo",
         action:   function action (){window.open('https://github.com/adan094')}
     },
     {
         text: "Gmail",
-        src: "../src/gmail.svg",
-        srcHover: "../src/gmailblue.svg",
+        src: "./gmail.svg",
+        srcHover: "./gmailblue.svg",
         alt: "Gmail Logo",
         action:   function action (){window.location = 'mailto:noahadamscs@gmail.com'}
     }
--- stackslist.js
+++ stackslist.js
@@ -1,7 +1,7 @@
 export default [
     {
         text: "React",
-        src: `${import.meta.env.BASE_URL}src/reactlogo.svg`,
+        src: `./reactlogo.svg`,
         alt: "React Logo"
     },
     {
@@ -11,37 +11,37 @@ export default [
     },
     {
         text: "CSS",
-        src: "../src/csslogo.svg",
+        src: "./csslogo.svg",
         alt: "CSS3 Logo"
     },
     {
         text: "JavaScript",
-        src: "../src/javascriptlogo.svg",
+        src: "./javascriptlogo.svg",
         alt: "JavaScript Logo"
     },
     {
         text: "SQL",
-        src: "../src/sqllogo.svg",
+        src: "./sqllogo.svg",
         alt: "SQL Logo"
     },
     {
         text: "C++",
-        src: "../src/c++logo.svg",
+        src: "./c++logo.svg",
         alt: "C++ Logo"
     },
     {
         text: "C",
-        src: "../src/clogo.svg",
+        src: "./clogo.svg",
         alt: "C Logo"
     },
     {
         text: "Java",
-        src: "../src/javalogo.svg",
+        src: "./javalogo.svg",
         alt: "Java Logo"
     },
     {
         text: "Python",
-        src: "../src/pythonlogo.svg",
+        src: "./pythonlogo.svg",
         alt: "Python Logo"
     }
 ]
