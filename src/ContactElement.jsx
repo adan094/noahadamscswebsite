c213e0f6cde03b4aa4d75bb894a71148789ae8fe
adan094 <adams128@uwindsor.ca>
2025-01-16 23:56:49 -0500

    fixed 2 visual bugs

--- src/ContactElement.jsx
+++ src/ContactElement.jsx
@@ -5,8 +5,8 @@ export default function ContactElement(props)
 
 
     return (
-                <div className="contactContainer" onClick={() => props.action()}  >
-                    <img alt={props.alt} onMouseEnter={()=>props.setHovered(true, props.id)} onMouseLeave={()=>props.setHovered(false, props.id)} src={props.isHovered?props.srcHover:props.src}/>
+                <div className="contactContainer" onClick={() => props.action()}  onMouseEnter={()=>props.setHovered(true, props.id)} onMouseLeave={()=>props.setHovered(false, props.id)} >
+                    <img alt={props.alt}  src={props.isHovered?props.srcHover:props.src}/>
                     <p>{props.text}</p>
                 </div> 
 
